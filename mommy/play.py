06c833c97641e34b07be84bb50df8b3a2cafe1e0
Brian Richardson <skinny@knowhere.net>
2017-02-24 20:18:26 -0800

    Some cleanups and improvements.

--- mommy/play.py
+++ mommy/play.py
@@ -25,7 +25,6 @@ snap_dir = "/Users/bzztbomb/projects/mame/snap/robotron"
 screenshot = os.path.join(snap_dir, "current_frame.png")
 moves = os.path.join(snap_dir, "moves.csv")
 
-think_frame = 0
 lines = []
 
 # Set up the detector with default parameters.
@@ -43,6 +42,9 @@ num_savior_frames = 9
 for i in range(num_savior_frames):
   savior.append(cv2.imread('reference/mutant' + str(i) + ".png"))
 
+video = None
+save_debug = False
+
 savior_loc = []
 while True:
   # Look
@@ -52,9 +54,15 @@ while True:
   while (image == None):
     try:
       image = cv2.imread(screenshot)
-      image = image[16:-12,8:-12]
+      image = image[17:-12,8:-12]
     except:
       pass
+
+  if (video == None) and (save_debug == True):
+    height, width, channels = image.shape
+    if (os.path.isfile('output.mp4')):
+      os.remove('output.mp4')
+    video = cv2.VideoWriter('output.mp4', -1, 10.0, (width,height))
 
   # Find the savior
   last_loc = savior_loc
@@ -77,6 +85,10 @@ while True:
   (thresh, im_bw) = cv2.threshold(img_gray, 16, 255, cv2.THRESH_BINARY)
   keypoints = detector.detect(im_bw)
   if (len(keypoints) > 0 and savior_loc != []):
+    im_with_keypoints = cv2.drawKeypoints(im_bw, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    if (video != None):
+      video.write(im_with_keypoints)
+
     keypoints = sorted(keypoints, key=lambda kp: math.hypot(kp.pt[0] - savior_loc[0], kp.pt[1] - savior_loc[1]))
     kp = keypoints[1 if len(keypoints) > 1 else 0]
     print("kp: " + str(kp.pt))
@@ -85,35 +97,26 @@ while True:
     y_index = 1
     movedownup = 1 if kp.pt[y_index] > savior_loc[y_index] else -1
     moveleftright = -1 if kp.pt[x_index] > savior_loc[x_index] else 1
-    firethresh = 20
+    firethresh = 15
     if abs(kp.pt[y_index] - savior_loc[y_index]) > firethresh:
       fireupdown = -movedownup
     else:
       fireupdown = 0
-    if abs(kp.pt[x_index] - savior_loc[x_index]) > firethresh:
+    if (abs(kp.pt[x_index] - savior_loc[x_index]) > firethresh) or (fireupdown == 0):
       fireleftright = -moveleftright
     else:
       fireleftright = 0
 
-    # Think
-    # movedownup = roborand()
-    # moveleftright  = roborand()
-    # fireupdown  = roborand()
-    # fireleftright  = roborand()
-    start = roborand()
     # Act
     lines = []
-    # lines.extend(port_to_moves(":IN0", ["Move Up", "Move Down", "Move Left", "Move Right", "1 Player Start", "2 Players Start", "Fire Up", "Fire Down"], port0))
-    # lines.extend(port_to_moves(":IN1", ["Fire Left", "Fire Right"], port1))
     lines.extend(axis_to_moves(":IN0", ["Move Down", "Move Up"], movedownup))
     lines.extend(axis_to_moves(":IN0", ["Move Left", "Move Right"], moveleftright))
     lines.extend(axis_to_moves(":IN0", ["Fire Down", "Fire Up"], fireupdown))
     lines.extend(axis_to_moves(":IN1", ["Fire Left", "Fire Right"], fireleftright))
+    start = roborand()
     lines.append(":IN0,1 Player Start," + ("1" if start > threshold else "0"))
   else:
     lines = []
-    # lines.extend(port_to_moves(":IN0", ["Move Up", "Move Down", "Move Left", "Move Right", "1 Player Start", "2 Players Start", "Fire Up", "Fire Down"], port0))
-    # lines.extend(port_to_moves(":IN1", ["Fire Left", "Fire Right"], port1))
     lines.extend(axis_to_moves(":IN0", ["Move Down", "Move Up"], 0))
     lines.extend(axis_to_moves(":IN0", ["Move Left", "Move Right"], 0))
     lines.extend(axis_to_moves(":IN0", ["Fire Down", "Fire Up"], 0))
@@ -121,11 +124,6 @@ while True:
     start = roborand()
     lines.append(":IN0,1 Player Start," + ("1" if start > threshold else "0"))
 
-
-  # print((moveupdown, moveleftright, fireupdown, fireleftright, start))
-  # print("\n")
-  # print(lines)
-  # print("\n")
   f = open(moves + ".tmp", "w")
   for line in lines:
     f.write(line)
@@ -133,19 +131,3 @@ while True:
   f.close()
   os.rename(moves + ".tmp", moves)
   os.remove(screenshot)
-  think_frame = think_frame + 1
-
-# PORT_START("IN0")
-# PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_UP ) PORT_NAME("Move Up")
-# PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_DOWN ) PORT_NAME("Move Down")
-# PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_LEFT ) PORT_NAME("Move Left")
-# PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_RIGHT ) PORT_NAME("Move Right")
-# PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_START1 )
-# PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_START2 )
-# PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_UP ) PORT_NAME("Fire Up")
-# PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_DOWN ) PORT_NAME("Fire Down")
-
-# PORT_START("IN1")
-# PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_LEFT ) PORT_NAME("Fire Left")
-# PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_RIGHT ) PORT_NAME("Fire Right")
-# PORT_BIT( 0xfc, IP_ACTIVE_HIGH, IPT_UNKNOWN )
